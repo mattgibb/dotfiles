15670269373c1d4dd438d52daeaeec930936d155
Matt Gibb <matt@mattgibb.com>
2013-12-30 21:50:28 +0100

    lein debugging and env vars

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,3 +1,23 @@
-{:user {:plugins [[lein-midje "3.0.0"]]
-         :dependencies [[slamhound "1.5.0"]]
-         :aliases {"slamhound" ["run" "-m" "slam.hound"]}}
+{:user {:plugins [[lein-midje "3.0.0"]
+                  [lein-environ "0.4.0"]]
+        :dependencies [[slamhound "RELEASE"]
+                       [org.clojars.gjahad/debug-repl "0.3.3"]]
+        :aliases {"slamhound" ["run" "-m" "slam.hound"]}
+        :injections [(defn contextual-eval [ctx expr]
+                       (eval   
+                         `(let [~@(mapcat (fn [[k v]] [k `'~v]) ctx)] ~expr)))
+                     (defmacro local-context []
+                       (let [symbols (keys &env)]
+                         (zipmap (map (fn [sym] `(quote ~sym)) symbols) symbols)))
+                     (defn readr [prompt exit-code]
+                       (let [input (clojure.main/repl-read prompt exit-code)]
+                         (if (= input ::tl)
+                           exit-code
+                           input)))
+                     ;;make a break point
+                     (defmacro break []
+                       `(clojure.main/repl
+                         :prompt #(print "debug=> ")
+                         :read readr
+                         :eval (partial contextual-eval (local-context))))
+                     ]}}
